c4a333e19914d63fade6258450ac802c0720043d
“John <johndoe@example.comclear>
2021-05-30 20:32:59 +0200

    A

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.set_page_config(layout="wide")
 
 app = MultiApp()
 
-imagen = Image.open("Tools/Images/logo.png")
+imagen = Image.open("Tools/Images/logo_cobi.png")
 st.sidebar.image(imagen)
 
 st.sidebar.markdown("""
